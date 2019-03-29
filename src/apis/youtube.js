affa070d16b9e4c3f02c86045828fce7a91f6ab2
Liz Burton <liz.burton147@gmail.com>
2019-03-29 08:18:04 -0400

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAJsGvxU95LciSWMWQ_ZY6maj-fJnit8DM'
+const KEY = ''
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
